de05e169b3f4cd08fbf7b197c66921eb5ee3c7e3
Abel <abelmathew1504@gmail.com>
2025-07-13 13:53:35 +0530

    Add sidebar component

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,92 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+const items = [
+  {
+    title: "Tasks",
+    url: "#",
+    icon: Home,
+  },
+]
+
+const categories = [
+  {
+    title: "Unassigned",
+    url: "#",
+  },
+  {
+    title: "Personal",
+    url: "#",
+  },
+  {
+    title: "Work",
+    url: "#",
+  },
+  {
+    title: "College",
+    url: "#",
+  },
+]
+ 
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <b>Welcome, Abel</b>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          {/* <SidebarGroupLabel>Application</SidebarGroupLabel> */}
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Categories</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {categories.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      {/* <item.icon /> */}
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        Developed by Abel Joby
+      </SidebarFooter>
+      {/* <SidebarRail /> */}
+    </Sidebar>
+  )
+}
